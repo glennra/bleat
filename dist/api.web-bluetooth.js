4f40793a0db3680af4da704feb77906b7be2707a
Glenn Ramsey <glennr@digitalrowing.com>
2018-10-12 12:02:37 +1300

    use correct check for undefined variable

--- dist/api.web-bluetooth.js
+++ dist/api.web-bluetooth.js
@@ -41,7 +41,7 @@
     } else {
         // Browser globals with support for web workers (root is window)
         // Assume Promise exists or has been poly-filled
-        if (cordovaApp) {
+        if (typeof cordovaApp !== 'undefined') {
             root.bleat = factory(root.Promise, root.Map, root.bleatHelpers);
         } else {
             root.bleat = root.navigator.bluetooth || factory(root.Promise, root.Map, root.bleatHelpers);
